921a70d207b4c11900d67c8eddf19dd79daeb2b8
SHOKUNBI AYODELE <shokunbiayodele@SHOKUNBIs-MacBook-Pro.local>
2025-10-07 10:42:42 +0100

    largest prime factor of any numbers

---
+++ larget_prime_factor.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function larget_prime($number) {
+    $i = 1;
+    $prime_number = array();
+    while ($i <= $number) {
+        if ($number % $i == 0) {
+            array_push($prime_number, $i);
+            $number /= $i;
+        }
+        $i++;
+    }
+
+    return largest_sort($prime_number);
+}
+
+function largest_sort($prime_number) {
+
+    $larget_number = 0;
+    for ($i = 1; $i <= count($prime_number); $i++) {
+        if ($prime_number[$i] > $larget_number) {
+            $larget_number = $prime_number[i + 1];
+        }
+    }
+
+    echo $larget_number;
+}
+
+
+   larget_prime(600851475143);
+
